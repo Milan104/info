d7b38ee3b6dd26732b0e8241cc078f79ba778490
Milan <milanimkohl@gmail.com>
2022-09-20 21:55:03 +0200

    clean up

--- Automat.py
+++ Automat.py
@@ -1,26 +1,21 @@
-def Ausgabe(input):
-    print((int(input) // 200),"x 2 Eurostücke werden ausgegben")
-    rest = int(input) % 200
-    if rest > 0:
-        print((int(rest) // 100),"x 1 Eurostücke werden ausgegben")
-        rest1 = (int(rest) % 100)
-        if rest1 > 0:
-            print((int(rest1) // 50),"x 50 Centstücke werden ausgegben")
-            rest2 = (int(rest1) % 50)
-            if rest2 > 0:
-                print((int(rest2) // 20),"x 20 Centstücke werden ausgegben")
-                rest3 = (int(rest2) % 20)
-                if rest3 > 0:
-                     print((int(rest2) // 10),"x 10 Centstücke werden ausgegben")
-                     rest4 = (int(rest3) % 10)
-                     if rest4 > 0:
-                         print((int(rest2) // 5),"x 5 Centstücke werden ausgegben")
-                         rest5 = (int(rest4) % 10)
-                         if rest5 > 0:
-                             print("Der Rest wird einbehalten 2 und 1 Centstücken sollten eh aus dem Verkehr gezogen werden")
-                        
-                     
-                     
-input = input("Geldsumme in ct angeben: ")
 
-Ausgabe(input)
+def rueckgeld(eingabe):
+    print((eingabe -(eingabe % 200))/200,"x 2 Eurostücke werden ausgegben")
+    rest1 = eingabe % 200
+    print((rest1-(rest1 % 100))/100,"x 1 Eurostücke werden ausgegben")
+    rest2 = rest1 % 100
+    print((rest2-(rest2 % 50))/50,"x 50ct Stücke werden ausgegben")
+    rest3 = rest2 % 50
+    print((rest3-(rest3 % 20))/20,"x 20ct Stücke werden ausgegben")
+    rest4 = rest3 % 20
+    print((rest4-(rest4 % 10))/10,"x 10ct Stücke werden ausgegben")
+    rest5 = rest4 % 10
+    print((rest5-(rest5 % 5))/5,"x 5ct Stücke werden ausgegben")
+    rest6 = rest5 % 5
+    print((rest6-(rest6 % 2))/2,"x 2ct Stücke werden ausgegben")
+    rest7 = rest6 % 2
+    print(rest7,"x 1ct Stücke werden ausgegben")
+
+eingabe = int(input("Geldsumme in ct angeben: "))
+rueckgeld(eingabe)
+
